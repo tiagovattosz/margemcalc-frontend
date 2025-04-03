da0d2a3115dec2e1d43348dd7652ecf80941d044
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-04-03 15:01:26 -0300

    Alterado Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,22 @@ import { Link } from "@tanstack/react-router";
 function Header() {
   return (
     <div className="container mx-auto p-4">
-      <div className="h-12 flex items-center justify-end gap-5 text-lg">
-        <Link to="/produtos" className="[&.active]:font-bold">
-          Produtos
+      <div className="h-12 flex items-center justify-between text-lg">
+        <Link to="/">
+          <img src="/icon.png" alt="Logo" className="h-12" />
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-        <Link to="/login" className="[&.active]:font-bold">
-          Login
-        </Link>
+
+        <div className="flex gap-5 text-lg">
+          <Link to="/produtos" className="[&.active]:text-blue-600">
+            Produtos
+          </Link>
+          <Link to="/about" className="[&.active]:text-blue-600">
+            About
+          </Link>
+          <Link to="/login" className="[&.active]:text-blue-600">
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );

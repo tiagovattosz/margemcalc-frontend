9ab80666767aa34a399c7c0e9058084b06194b44
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-04-03 15:01:05 -0300

    Instalado MaterialUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./tailwind.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";

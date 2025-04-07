bb8681b638ee4173f4b881269c98eaa161e48ab7
Tiago Vattos Zamboni <tiagovattosz@gmail.com>
2025-04-07 15:22:10 -0300

    Adicionado detalhes do produto no card

--- src/routes/produtos/index.jsx
+++ src/routes/produtos/index.jsx
@@ -6,6 +6,7 @@ import {
   Card,
   CardContent,
   IconButton,
+  CircularProgress,
 } from "@mui/material";
 import { Edit, Delete } from "@mui/icons-material";
 
@@ -15,6 +16,17 @@ export const Route = createFileRoute("/produtos/")({
 
 function RouteComponent() {
   const [products, setProducts] = useState([]);
+  const [loading, isLoading] = useState(true);
+
+  function formatarMoeda(valor) {
+    if (!valor) {
+      return "";
+    }
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
 
   async function handleDeleteClick(productId) {
     try {
@@ -41,6 +53,7 @@ function RouteComponent() {
         });
         const data = await response.json();
         setProducts(data);
+        isLoading(false);
       } catch (e) {
         console.error("Erro ao listar produtos");
         console.error(e);
@@ -50,49 +63,101 @@ function RouteComponent() {
     fetchProdutos();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="w-screen flex mt-30 justify-center">
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
     <Container maxWidth="md" className="mb-4">
       <div className="flex justify-center mb-4">
-        <Button variant="contained">
-          <Link to="/produtos/novo">Novo Produto</Link>
+        <Button
+          variant="contained"
+          fullWidth
+          disableElevation
+          component={Link}
+          to="/produtos/novo"
+        >
+          Novo Produto
         </Button>
       </div>
 
       <div className="flex flex-col gap-2">
-        {products.map((product) => (
-          <Card variant="outlined" key={product.id}>
-            <CardContent className="flex justify-between items-center">
-              <p className="text-lg">
-                {product.id} - {product.nome}
-              </p>
+        {products.map((product) => {
+          return (
+            <Card variant="outlined" key={product.id}>
+              <CardContent>
+                <div className="flex justify-between items-center">
+                  <p className="font-semibold text-lg">{product.nome}</p>
 
-              <div className="flex gap-2">
-                <IconButton
-                  component={Link}
-                  to={`/produtos/${product.id}/editar`}
-                  color="primary"
-                >
-                  <Edit />
-                </IconButton>
+                  <div className="flex gap-2">
+                    <IconButton
+                      component={Link}
+                      to={`/produtos/${product.id}/editar`}
+                      color="primary"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                      }}
+                    >
+                      <Edit />
+                    </IconButton>
 
-                <IconButton
-                  color="error"
-                  onClick={() => {
-                    if (
-                      window.confirm(
-                        `Deseja excluir o produto "${product.nome}"?`
-                      )
-                    ) {
-                      handleDeleteClick(product.id);
+                    <IconButton
+                      color="error"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        if (
+                          window.confirm(
+                            `Deseja excluir o produto "${product.nome}"?`
+                          )
+                        ) {
+                          handleDeleteClick(product.id);
+                        }
+                      }}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </div>
+                </div>
+
+                <p>Preço de compra: {formatarMoeda(product.precoCompra)}</p>
+
+                <p>Preço de venda: {formatarMoeda(product.precoVenda)}</p>
+
+                <p className="mt-4 font-semibold text-lg">Comissões e taxas</p>
+
+                <p>
+                  Comissão: {formatarMoeda(product.valorTotalComissaoCompra)} (
+                  {product.porcentagemComissaoCompra}% +{" "}
+                  {formatarMoeda(product.valorFixoComissaoCompra)})
+                </p>
+
+                <p>
+                  Taxa: {formatarMoeda(product.valorTotalTaxaVenda)} (
+                  {product.porcentagemTaxaVenda}% +{" "}
+                  {formatarMoeda(product.valorFixoTaxaVenda)})
+                </p>
+
+                <p className="mt-4 font-semibold text-lg">Margens</p>
+
+                <p>
+                  Lucro Final:{" "}
+                  <span
+                    className={
+                      product.lucroFinal > 0 ? "text-green-600" : "text-red-600"
                     }
-                  }}
-                >
-                  <Delete />
-                </IconButton>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+                  >
+                    {formatarMoeda(product.lucroFinal)} (
+                    {product.porcentagemLucroFinal}%)
+                  </span>{" "}
+                </p>
+              </CardContent>
+            </Card>
+          );
+        })}
       </div>
     </Container>
   );
